ccf77809149618e6c7e4fc0b21381cb2322349a2
zhengzha <zhengzha>
2010-02-02 21:34:36 +0000

    dump mysql db schema; sql translator based migrate to psql

---
+++ scripts/dump.sh
@@ -0,0 +1,1 @@
+mysqldump -uzheng -pweigaocn modencode_mart --no-data=true --add-drop-table=false > modencode_mart_schema.mysql
---
+++ scripts/migrate.sh
@@ -0,0 +1,1 @@
+sqlt -f MySQL -t PostgreSQL modencode_mart_schema.mysql > modencode_mart_schema.psql
