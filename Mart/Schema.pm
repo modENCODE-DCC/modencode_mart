2fe215a0fe78f6d3569215f79d953186f043f825
zhengzha <zhengzha>
2010-02-05 20:32:08 +0000

    i forgot to commit this base module

---
+++ Mart/Schema.pm
@@ -0,0 +1,16 @@
+package Schema;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_classes;
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-02-02 14:00:25
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pj5CeBVvZVM3zRglSsMImQ
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
