f4b663584dc2449af2c13824c29c60ec3130f09f
zhengzha <zhengzha>
2010-03-23 19:30:58 +0000

    add support to tissue. will not support cellline since cellline = devstage + tissue.

--- modencode_mart_load.pl
+++ modencode_mart_load.pl
@@ -20,20 +20,15 @@ use GEO::LWReporter;
 use Loader::TransfacTranscriptionalFactorMain_Loader;
 use Loader::TransfacBindingSitesMain_Loader;
 use Loader::TransfacDevelopmentalStageDm_Loader;
+use Loader::TransfacTissueDm_Loader;
 use Pipeline::Pipeline;
 
 print "initializing...\n";
-my ($unique_id, $config, $no_insert, $no_create, $force, $gff);
-$no_insert=1;
-$no_create=1;
-$force=0;
+my ($unique_id, $config, $gff);
 $config = $root_dir . 'config/modencode_mart.ini';
 my $option = GetOptions ("unique_id=s"    => \$unique_id,
                          "config=s"       => \$config,
-			 "no_insert=i"      => \$no_insert,
-			 "no_create=i"      => \$no_create,
-			 "force_recreate=i" => \$force,
-			 "gff=s" => \$gff) or usage();
+			 "gff=s"          => \$gff) or usage();
 usage() unless defined($unique_id);
 usage() unless -e $config;
 my %ini;
@@ -44,23 +39,18 @@ my $pipe = new Pipeline::Pipeline({
 my ($reader, $experiment) = $pipe->load_experiment($unique_id);
 print $experiment->to_string();
 
-####ok##########
 my $reporter = new GEO::LWReporter({
     'unique_id' => $unique_id,
     'reader' => $reader,
     'experiment' => $experiment});
 $reporter->set_all();
-######need ModENCODE::Chado::Data object for tgt_gene, strain, devstage, cellline, etc.
 
 my $species = $reporter->get_organism();
 my $pname = $reporter->get_tgt_gene();
 my $devstage = $reporter->get_devstage();
+my $tissue = $reporter->get_tissue();
 my $sex = $reporter->get_sex();
 my $antibody = $reporter->get_antibody();
-print $pname;
-print $devstage;
-print $sex;
-print $antibody;
 $pname = $antibody unless $pname;
 
 my $tfl = new Loader::TransfacTranscriptionalFactorMain_Loader({
@@ -71,27 +61,28 @@ my $tfl = new Loader::TransfacTranscriptionalFactorMain_Loader({
 });
 $tfl->info;
 my $tf = $tfl->load; #a tf table dbix::class 
-for my $col (qw[tf_id_key
-                dcc_id
-                species 
-                gene_id 
-                public_name
-                tf_chromosome_name
-                tf_chromosome_start
-                tf_chromosome_end
-                tf_chromosome_strand
-                ]) {
-    print $tf->$col, "\n";
+
+if ($devstage) {
+    my $devl = new Loader::TransfacDevelopmentalStageDm_Loader({
+	config => \%ini,
+	tf_id_key => $tf->tf_id_key,
+	official_name => $devstage,
+	species => $species,
+	sex => $sex
+    });
+    $devl->load();
 }
 
-my $devl = new Loader::TransfacDevelopmentalStageDm_Loader({
-    config => \%ini,
-    tf_id_key => $tf->tf_id_key,
-    official_name => $devstage,
-    species => $species,
-    sex => $sex
-});
-$devl->load();
+if ($tissue) {
+    my $tl = new Loader::TransfacTissueDm_Loader({
+	config => \%ini,
+	tf_id_key => $tf->tf_id_key,
+	official_name => $tissue,
+	species => $species,
+	sex => $sex	
+    });
+    $tl->load();
+}
 
 my $bsl = new Loader::TransfacBindingSitesMain_Loader({
     config => \%ini,
@@ -102,8 +93,6 @@ my $bsl = new Loader::TransfacBindingSitesMain_Loader({
 $gff = $pipe->download_gff() unless $gff;
 $bsl->load($gff);
 
-###cool till this, loaded tf, devstage, bs, bs_gene 4 table!!!
-###shall try the mart interface.
 
 sub usage {
     my $usage = qq[$0 -unique_id <submission_id> -config <config_file> --no_insert <0|1> --no_create <0|1> --force_recreate <0|1>];
