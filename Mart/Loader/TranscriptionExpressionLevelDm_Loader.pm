be1c0e3560d478bc638644310d8da4c236841089
zhengzha <zhengzha>
2011-05-17 18:39:56 +0000

    three loader for transcriptome data. notice the feature-feature loader will be used for calc relationship between all transcripts and annotated features. this is a generalization of transfac bindingsite - feature loading module.

--- Mart/Loader/TranscriptionExpressionLevelDm_Loader.pm
+++ Mart/Loader/TranscriptionExpressionLevelDm_Loader.pm
@@ -5,20 +5,15 @@ package Loader::TranscriptionFeatureMain_Loader;
 use strict;
 use warnings;
 use Carp qw/croak/;
-use Data::Dumper;
 use Class::Std;
 use base 'Loader';
 use Schema;
-use GFF3::GFF3Rec;
-use Loader::TranscriptionFeatureFeatureDm_Loader;
 
 my %dcc_id         :ATTR( :name<dcc_id>        :default<undef>);
 my %config         :ATTR( :name<config>        :default<undef>);
 my %species        :ATTR( :name<species>       :default<undef>);
-my %devstage       :ATTR( :name<devstage>      :default<undef>);
-my %tissue         :ATTR( :name<tissue>        :default<undef>);
-my %sex            :ATTR( :name<sex>           :default<undef>);
 my %db             :ATTR( :name<db>            :default<undef>);
+
 
 sub BUILD {
     my ($self, $ident, $args) = @_;
@@ -28,13 +23,6 @@ sub BUILD {
         my $f = "set_" . $p;
         $self->$f($v);
     }
-    for my $p (qw[devstage tissue sex]) {
-        my $v = $args->{$p};
-	if (defined $v) {
-	    my $f = "set_" . $p;
-	    $self->$f($v);
-	}
-    }    
     my $db = $self->connect_gff();
     $self->set_db($db);
     return $self;
@@ -44,19 +32,7 @@ sub load {
     my $self = shift;
     #use the 'mart' part in config
     my $schema = Schema->connect($self->connectinfo('mart'));
-    my $annodbh = $self->get_db();
-    my @columns = qw[dcc_id
-                     species
-                     ts_chromosome_name
-                     ts_chromosome_start
-                     ts_chromosome_end
-                     ts_chromosome_strand
-                     ts_type
-                     assembly_version
-                     ts_length
-                     prediction_status
-                    ];
-    my $p = __PACKAGE__; $p =~ s/^Loader:://; $p =~ s/_Loader$//;
+    
 }
 
 1;
