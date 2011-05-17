8fca5cc3f4cedcb34f8618962f2e8b63001b3694
zhengzha <zhengzha>
2011-05-17 18:35:06 +0000

    first checkin of loading transcription feature main table.

--- Mart/Schema/TranscriptionFeatureMain.pm
+++ Mart/Schema/TranscriptionFeatureMain.pm
@@ -41,12 +41,37 @@ __PACKAGE__->add_columns(
     is_nullable => 0,
     size => 10,
   },
+  "ts_length",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 10 },
+  "prediction_status",
+  { data_type => "TINYINT", default_value => undef, is_nullable => 0, size => 1 },
+  "devstage",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 50,
+  },
+  "tissue",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 50,
+  },
+  "sex",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 20,
+  },
 );
 __PACKAGE__->set_primary_key("ts_id_key");
 
 
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-03-14 11:21:47
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Mo7zNGqU3GEj15GwAP5vwA
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-05-17 12:41:24
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nwJxh091DnAaBaWVmPtGSw
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
