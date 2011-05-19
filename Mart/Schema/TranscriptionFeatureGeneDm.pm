103c6c7a38b510a02fa5ff83dc86f54bcdf2ca2d
zhengzha <zhengzha>
2011-05-19 17:15:12 +0000

    rename columns to make them the same across all dm tables, so the loader could be generalized.

--- Mart/Schema/TranscriptionFeatureGeneDm.pm
+++ Mart/Schema/TranscriptionFeatureGeneDm.pm
@@ -10,34 +10,57 @@ __PACKAGE__->table("transcription__feature_gene__dm");
 __PACKAGE__->add_columns(
   "ts_id_key",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 20 },
-  "gene_id",
+  "ts_gene_id",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 20 },
-  "gene_name",
+  "feature_id",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 20,
+  },
+  "feature_name",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 64,
   },
-  "gene_chromosome",
+  "feature_type",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 20,
+  },
+  "feature_chromosome",
   {
     data_type => "VARCHAR",
     default_value => undef,
     is_nullable => 0,
     size => 10,
   },
-  "gene_start",
+  "feature_start",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 20 },
-  "gene_end",
+  "feature_end",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 20 },
-  "gene_strand",
+  "feature_strand",
   { data_type => "CHAR", default_value => undef, is_nullable => 0, size => 1 },
+  "relative_position",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 10,
+  },
+  "distance",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 20 },
 );
-__PACKAGE__->set_primary_key("gene_id");
+__PACKAGE__->set_primary_key("ts_gene_id");
 
 
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-05-17 12:41:24
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wsXUpy9T8MFTQX/Z4uNLgg
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2011-05-19 11:29:57
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:1BUJF0R58gYnIhBP5eQseg
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
