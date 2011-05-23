8e5231d401dd976d37ddb2960860763b196dba70
zhengzha <zhengzha>
2011-05-23 23:38:09 +0000

    update a little bit.

--- Mart/Loader/TranscriptionFeatureFeatureDm_Loader.pm
+++ Mart/Loader/TranscriptionFeatureFeatureDm_Loader.pm
@@ -14,12 +14,13 @@ use base 'Loader';
 use Schema;
 
 my %config    :ATTR(:name<config>     :default<undef>);
-my %species   :ATTR(:name<species>       :default<undef>);
-my %bs_id_key :ATTR(:name<bs_id_key>  :default<undef>);
+my %ft_id_key :ATTR(:name<ft_id_key>  :default<undef>);
+my %ft        :ATTR(:name<ft>         :default<undef>);
+my %dbh       :ATTR(:name<dbh>        :default<undef>);
 
 sub BUILD {
     my ($self, $ident, $args) = @_;
-    for my $p (qw[config species bs_id_key]) {
+    for my $p (qw[config species ft_id_key ft]) {
 	my $v = $args->{$p};
 	defined $v || croak 'need parameter $p';
 	my $f = "set_" . $p;
@@ -30,64 +31,72 @@ sub BUILD {
 
 sub load {
     #accept a bio-db-seqfeature handle, a gff3rec obj.
-    my ($self, $db, $rec, @types) = @_;
-    for my $type (@types) {
-	$type = lc($type);
-	my $uctype = ucfirst($type);
-	my $schema = Schema->connect($self->connectinfo('mart'));
-	my $p = __PACKAGE__; $p =~ s/^Loader:://; $p =~ s/_Loader$//; $p =~ s/Features/$uctype/;
-	my $bsg_rs = $schema->resultset($p);    
-
-	my @columns = qw[bs_id_key
-                         feature_id
-                         feature_public_name
-                         distance
-                         relative_position
-                         ];
-
-	my $cutoff; 
-	$cutoff = $self->get_config()->{'mart'}{'distance_cutoff'} if $type eq 'gene';
-	$cutoff = $self->get_config()->{'mart'}{'exon_intron_distance_cutoff'} if $type eq 'exon' || $type eq 'intron';
-
-	my ($chr, $bs_st, $bs_end) = ($rec->get_seqid(),
-				      $rec->get_start(),
-				      $rec->get_end()
-	    );
-	my $bs_pt = bs_pt($bs_st, $bs_end);
-	my $bs_len = abs($bs_st-$bs_end+1);
-	my $rg_min = $bs_pt - $cutoff > 0 ? $bs_pt - $cutoff : 0;
-	my $rg_max = $bs_pt + $cutoff;
-	print "region: center $bs_pt, min $rg_min, max $rg_max\n";
-        #more location, feature type control could go into here
-	my $it = $db->segment($chr, $rg_min, $rg_max)->get_seq_stream(-type => $type);
-	while (my $ft = $it->next_seq) {
-	    if ( defined($ft->id) ) { # NO landmark genes
-		my $dst;
-		$dst = bs_ft_tss_dst($bs_pt, $ft) if $type eq 'gene';
-		$dst = bs_ft_midpt_dst($bs_pt, $bs_len, $ft) if $type eq 'exon' || $type eq 'intron';
-		print join(" ", ('Gene', $ft->id, $ft->load_id, 'distance', $dst, "\n"));
-		my ($load_id, $display_name, $rp);
-		$load_id = defined($ft->load_id) ? $ft->load_id : undef;
-		$display_name = defined($ft->display_name) ? $ft->display_name : undef;
-		($dst, $rp) = bs_ft_tss_dst($bs_pt, $ft);
-		my @data = ($self->get_bs_id_key,
-			    $load_id,
-			    $display_name,
-			    $dst,
-			    $rp
-		    );
-		$bsg_rs->populate([\@columns,
-				   \@data
-				  ]);
+    my ($tbl, $type) = @_;
+    my $schema = Schema->connect($self->connectinfo('mart'));
+    my $ffd_rs = $schema->resultset($tbl);    
+    my @columns = qw[ts_id_key
+                     feature_id
+                     feature_name
+                     feature_type
+                     feature_chromosome
+                     feature_start
+                     feature_end
+                     feature_strand
+                     distance
+                     relative_position
+                    ];
+ 
+    my %cutoff_map = (gene => gene_distance_cutoff,
+		      exon => exon_distance_cutoff,
+		      intron => intron__distance_cutoff,
+		      CDS => cds__distance_cutoff,
+		      transcript => transcript__distance_cutoff,
+		      polyA_site => polya_distance_cutoff,
+		      TSS => tss_distance_cutoff,
+		      transcription_end_site => tes_distance_cutoff,
+		      SL1_acceptor_site => sl1_distance_cutoff,
+		      SL2_acceptor_site => sl2_distance_cutoff
+	);
+    my $cutoff = $cutoff_map{$type};
+    
+    my ($chr, $st, $end) = ($rec->get_seqid(),
+			    $rec->get_start(),
+			    $rec->get_end()
+	);
+    my $pt = int(($st+$end)/2);
+    my $len = abs($st-$end+1);
+    my $rg_min = $pt - $cutoff > 0 ? $pt - $cutoff : 0;
+    my $rg_max = $pt + $cutoff;
+    print "region: center $pt, min $rg_min, max $rg_max\n";
+    #more location, feature type control could go into here
+    my $it = $db->segment($chr, $rg_min, $rg_max)->get_seq_stream(-type => $type);
+    while (my $ft = $it->next_seq) {
+	if ( defined($ft->id) ) { # NO landmark genes
+	    my $dst;
+#	    $dst = bs_ft_tss_dst($bs_pt, $ft) if $type eq 'gene';
+#	    $dst = bs_ft_midpt_dst($bs_pt, $bs_len, $ft) if $type eq 'exon' || $type eq 'intron';
+	    print join(" ", ('Gene', $ft->id, $ft->load_id, 'distance', $dst, "\n"));
+	    my ($load_id, $display_name, $rp);
+	    $load_id = defined($ft->load_id) ? $ft->load_id : undef;
+	    $display_name = defined($ft->display_name) ? $ft->display_name : undef;
+	    #($dst, $rp) = bs_ft_tss_dst($bs_pt, $ft);
+	    my @data = ($self->get_ft_id_key,
+			$load_id,
+			$display_name,
+			$ft->seqid,
+			$ft->start,
+			$ft->end,
+			$ft->strand,
+			$dst,
+			$rp
+		);
+	    $ffd_rs->populate([\@columns,
+			       \@data
+			      ]);
 	    }
 	}
     }
 }
-
-sub bs_pt {
-    my ($st, $end) = @_;
-    return ($st+$end)/2;
-} 
 
 sub bs_ft_tss_dst {
     my ($bs_pt, $ft) = @_;
